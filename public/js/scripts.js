4b0137171e554b7c098c66cadbe604834764c2b3
ChoudharyNavit22 <navitchoudhary22@gmail.com>
2022-08-03 18:44:10 +1000

    Updates - added modal - added submit form feature

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -27,7 +27,25 @@ const addCards = (items) => {
     });
 }
 
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+
+    console.log("Form Data Submitted: ", formData);
+}
+
 $(document).ready(function(){
     $('.materialboxed').materialbox();
     addCards(cardList);
+    $('.modal').modal();
+    $('#formSubmit').click(()=>{
+        submitForm();
+    })
 })
